1f779c9be8847a7b50306092fe9dc3e31fef3e7c
mpove <130360685+mpove@users.noreply.github.com>
2024-08-12 16:12:16 +0200

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-// Scroll suave
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
